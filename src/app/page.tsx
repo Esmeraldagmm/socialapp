4f4bef049ea04014a59a2ab6aed8d20f85ddc277
Esmeralda Gonzalez <esmeraldamenera27@gmail.com>
2025-03-02 00:42:17 -0500

    Create posts and added hot toast

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { SignInButton, UserButton, SignedIn, SignedOut } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import ModeToggle from "@/components/ModeToggle";
+import { currentUser } from "@clerk/nextjs/server";
+import CreatePost from "@/components/CreatePost";
 
-export default function Home() {
+export default async function Home() {
+  const user = await currentUser();
   return (
-    <div className="m-4">
-      <h1>Home Page Content</h1>
+    <div className="grid grid-cols-1 lg:grid-cols-10 gap-6">
+      <div className="lg:col-span-6">{user ? <CreatePost /> : null}</div>
+      <div className="hiddne lg:block lg:col-span-4 sticky top-20">
+        whotofollow
+      </div>
     </div>
   );
 }
